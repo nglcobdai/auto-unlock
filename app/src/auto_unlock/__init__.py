f8093c47fc1b417add542cd908124e9675963a61
KodaiYamashita <nglcobdai@gmail.com>
2024-06-22 16:24:17 +0900

    fix: path

--- app/src/auto_unlock/__init__.py
+++ app/src/auto_unlock/__init__.py
@@ -1,5 +1,5 @@
-from auto_unlock_app import AutoUnlockApp
-from auto_unlock_app_w_auth import AutoUnlockAppWAuth
+from app.src.auto_unlock.auto_unlock_app import AutoUnlockApp
+from app.src.auto_unlock.auto_unlock_app_w_auth import AutoUnlockAppWAuth
 
 
 def auto_unlock_app(is_authenticating=True):
