d741e36ee29f71070ea05831d3a0e2360d8337ca
KodaiYamashita <nglcobdai@gmail.com>
2024-09-16 15:50:57 +0900

    fix: log

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,7 +1,7 @@
 from app.utils.config import Settings
-from app.utils.log import Logger
+from app.utils.log import get_logger
 from app.utils.slack import Slack
 
 settings = Settings()
-logger = Logger(settings.PROJECT_NAME).logger
+logger = get_logger(settings.PROJECT_NAME)
 slack = Slack(settings.SLACK_API_TOKEN)
