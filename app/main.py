6087824ee19c843731bf217aa6e8007681fce8a8
KodaiYamashita <nglcobdai@gmail.com>
2024-10-06 14:33:22 +0900

    fix: logger -----

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from app.src import auto_unlock_app
-from app.utils import settings
+from app.utils import settings, logger
 
 
 def main():
+    logger.info("----------------------------------------")
     auto_unlock_app(settings.IS_AUTHENTICATION)
+    logger.info("----------------------------------------")
 
 
 if __name__ == "__main__":
